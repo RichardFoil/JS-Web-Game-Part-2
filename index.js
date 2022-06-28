c0acb6be22f637104664f51f98bccbe70bbf1446
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-06-28 19:37:07 -0400

    finished activity and bonus

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
+    return item
 }
+
+newItem('assets/sword.png', 500, 555)
+newItem('assets/shield.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
+
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -26,6 +38,7 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
 newInventory()
@@ -36,6 +49,6 @@ newImage('assets/pine-tree.png', 450, 350)
 newImage('assets/crate.png', 150, 350)
 newImage('assets/well.png', 500, 575)
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+
+
+const inventory = newInventory()
